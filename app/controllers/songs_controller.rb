dd931c78d70b9f4d2d62e75ae4c9ca9abee55a62
Dawit Gizaw <gizawdawit123.com>
2020-04-06 13:36:48 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,23 +1,21 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
-    @song = Song.new
   end
 
   def create
     @song = Song.new(song_params)
     @song.artist = Artist.find_or_create_by(artist_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :new
-    end
+    
+    redirect_to song_path(@song)
+    
   end
 
   def edit
